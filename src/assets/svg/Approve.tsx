1f33984d4c6758feb4aef36c6d734e5b1428316b
Ksenia <ksenia.pilguy@scrumlaunch.com>
2023-07-25 18:21:02 +0300

    fixed svg props

--- src/assets/svg/Approve.tsx
+++ src/assets/svg/Approve.tsx
@@ -3,7 +3,7 @@ const Approve = () => {
   return (
     <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
       <circle cx="10" cy="10.5" r="10" fill="#00844A"/>
-      <path d="M14.4443 7.1665L8.3332 13.2776L5.55542 10.4998" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M14.4443 7.1665L8.3332 13.2776L5.55542 10.4998" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
     </svg>
   )
 }
