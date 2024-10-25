091ce59ddc502cd4a5ab357dbc4c7c166aabb102
Bruno Reis <bruno.reis@scrumlaunch.com>
2024-10-25 09:15:25 -0300

    - firstName and lastName merged - display current and new roles - phone number formatted - birthdate formatted (without tz) - "skip for this" going to next duplicate

--- src/pages/Protected/Users/components/UsersReviewUpdateModal/components/UsersDetailsColumn.tsx
+++ src/pages/Protected/Users/components/UsersReviewUpdateModal/components/UsersDetailsColumn.tsx
@@ -1,10 +1,12 @@
 import styled from '@emotion/styled'
 import { Flex, Typography } from 'antd'
-import { FC } from 'react'
+import { FC, useCallback } from 'react'
 
 import { FULL_GENDER_NAMES } from '@/common/constants'
-import { IFENew } from '@/common/interfaces/user'
-import { TGender } from '@/common/types'
+import { IAsEntity, IChildren, IExtendedFEUser, IFENew, IOperator } from '@/common/interfaces/user'
+import { TGender, TRole } from '@/common/types'
+import { IIdName } from '@/common/interfaces'
+import { formatPhoneNumber } from '@/utils'
 
 const Container = styled(Flex)<{ is_new: string }>`
   flex: 1 1 50%;
@@ -34,58 +36,132 @@ const ItemValueStyle = styled(Typography)<{ is_changed: string }>`
 interface IUsersDetailsColumnProps extends IFENew {
   title: string
   isNew: boolean
+  // Since we have to show current roles at the same column
+  // as imported data, we need to pass current user data to use when isNew is true
+  current?: IExtendedFEUser
   differences: Record<keyof IFENew, boolean>
 }
 
-const UsersDetailsColumn: FC<IUsersDetailsColumnProps> = ({
+type TUsersDetailsColumnProps = {
+  asCoach?: IAsEntity | null
+  asPlayer?: IAsEntity | null
+  operator?: IOperator | null
+  asHeadCoach?: IIdName[] | null
+  asTeamAdmin?: IIdName[] | null
+  birthDateFormatted?: string
+  isChild?: boolean
+  asParent?: null | IChildren[]
+} & IUsersDetailsColumnProps
+
+type RoleTeamsMap = Record<TRole | 'Parent', IIdName[] | IOperator | IChildren[] | null>;
+
+const UsersDetailsColumn: FC<TUsersDetailsColumnProps> = ({
   isNew,
   title,
-  birthDate,
+  birthDateFormatted,
   firstName,
-  gender,
+  gender= 2,
   lastName,
   phoneNumber,
   zipCode,
   email = '',
-}) => (
-  <Container is_new={`${isNew}`}>
-    <Title>{title}</Title>
+  roles,
+  teams,
+  current,
+  differences,
+  asPlayer = null,
+  asCoach = null,
+  asParent = null,
+  asHeadCoach = null,
+  operator = null,
+  asTeamAdmin = null,
+}) => {
+  const currentRoles = isNew ? current?.roles : roles
 
-    <Flex className="mg-b16" vertical>
-      <ItemTitle is_changed={`false`}>First Name:</ItemTitle>
-      <ItemValueStyle is_changed={`false`}>{firstName}</ItemValueStyle>
-    </Flex>
+  const teamsAsText = useCallback((role: TRole | 'Parent'): string | null => {
+    const roleTeamsMap: RoleTeamsMap = {
+      'Coach': current?.asCoach?.teams || asCoach?.teams || null,
+      'Player': current?.asPlayer?.teams || asPlayer?.teams || null,
+      'Head Coach': current?.asHeadCoach || asHeadCoach,
+      'Team Admin': current?.asTeamAdmin || asTeamAdmin,
+      'Master Admin': null,
+      'Operator': current?.operator || operator ,
+      'Parent': current?.asParent || asParent
+    }
 
-    <Flex className="mg-b16" vertical>
-      <ItemTitle is_changed={`false`}>Last Name:</ItemTitle>
-      <ItemValueStyle is_changed={`false`}>{lastName}</ItemValueStyle>
-    </Flex>
+    const teamsOrOperator = roleTeamsMap[role]
 
-    <Flex className="mg-b16" vertical>
-      <ItemTitle is_changed={`false`}>Gender:</ItemTitle>
-      <ItemValueStyle is_changed={`false`}>{gender ? FULL_GENDER_NAMES[gender as TGender] : '-'}</ItemValueStyle>
-    </Flex>
+    if (Array.isArray(teamsOrOperator)) {
+      if (role === 'Parent') {
+        const parents = teamsOrOperator as IChildren[]
+        return parents.map((parent) => `${parent.firstName} ${parent.lastName}`).join(', ')
+      }
 
-    <Flex className="mg-b16" vertical>
-      <ItemTitle is_changed={`false`}>Email:</ItemTitle>
-      <ItemValueStyle is_changed={`false`}>{email || '-'}</ItemValueStyle>
-    </Flex>
+      return (teamsOrOperator as IIdName[]).map((team) => team.name).join(', ')
+    }
 
-    <Flex className="mg-b16" vertical>
-      <ItemTitle is_changed={`false`}>Birth Date:</ItemTitle>
-      <ItemValueStyle is_changed={`false`}>{birthDate || '-'}</ItemValueStyle>
-    </Flex>
+    if (teamsOrOperator && 'name' in teamsOrOperator) {
+      return teamsOrOperator.name
+    }
 
-    <Flex className="mg-b16" vertical>
-      <ItemTitle is_changed={`false`}>Phone:</ItemTitle>
-      <ItemValueStyle is_changed={`false`}>{phoneNumber}</ItemValueStyle>
-    </Flex>
+    // Return undefined if there are no teams or operator
+    return null
+  }, [asCoach, asPlayer, asHeadCoach, asTeamAdmin, operator, asParent, current])
 
-    <Flex className="mg-b16" vertical>
-      <ItemTitle is_changed={`false`}>Zip Code:</ItemTitle>
-      <ItemValueStyle is_changed={`false`}>{zipCode || '-'}</ItemValueStyle>
-    </Flex>
-  </Container>
-)
+  return (
+    <Container is_new={`${isNew}`}>
+      <Title>{title}</Title>
+
+      <Flex className="mg-b16" vertical>
+        <ItemTitle is_changed={`false`}>Name:</ItemTitle>
+        <ItemValueStyle is_changed={`false`}>{firstName} {lastName}</ItemValueStyle>
+      </Flex>
+
+      <Flex className="mg-b16" vertical>
+        <ItemTitle is_changed={`false`}>Gender:</ItemTitle>
+        <ItemValueStyle is_changed={`false`}>{gender ? FULL_GENDER_NAMES[gender as TGender] : '-'}</ItemValueStyle>
+      </Flex>
+
+      <Flex className="mg-b16" vertical>
+        <ItemTitle is_changed={`false`}>Email:</ItemTitle>
+        <ItemValueStyle is_changed={`false`}>{email || '-'}</ItemValueStyle>
+      </Flex>
+
+      <Flex className="mg-b16" vertical>
+        <ItemTitle is_changed={`false`}>Birth Date:</ItemTitle>
+        <ItemValueStyle is_changed={`false`}>{birthDateFormatted}</ItemValueStyle>
+      </Flex>
+
+      <Flex className="mg-b16" vertical>
+        <ItemTitle is_changed={`false`}>Phone:</ItemTitle>
+        <ItemValueStyle is_changed={`false`}>{phoneNumber ? formatPhoneNumber(phoneNumber) : '-'}</ItemValueStyle>
+      </Flex>
+
+      <Flex className="mg-b16" vertical>
+        <ItemTitle is_changed={`false`}>Zip Code:</ItemTitle>
+        <ItemValueStyle is_changed={`false`}>{zipCode || '-'}</ItemValueStyle>
+      </Flex>
+
+      <Flex className="mg-b16" vertical>
+        <ItemTitle is_changed={`false`}>Current Roles:</ItemTitle>
+        {!currentRoles?.length && (
+          <ItemValueStyle is_changed={`false`}>-</ItemValueStyle>
+        )}
+        {currentRoles?.map(role => (
+          <ItemValueStyle key={`${role}-key`} is_changed={`false`}>{role}: {teamsAsText(role as TRole)};</ItemValueStyle>
+        ))}
+      </Flex>
+
+      {isNew && current && differences.roles && (
+        <Flex className="mg-b16" vertical>
+          <ItemTitle is_changed={`${isNew}`}>New Roles:</ItemTitle>
+          {roles?.map((role, index) => (
+            <ItemValueStyle key={`new-${role}-key`} is_changed={`${isNew}`}>{role}: {teams[index]};</ItemValueStyle>
+          ))}
+        </Flex>
+      )}
+    </Container>
+  )
+}
 
 export default UsersDetailsColumn
