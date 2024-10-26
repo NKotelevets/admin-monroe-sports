2e894bfcbcb60115c999dc56f2286ca4406d9230
Bruno Reis <bruno.reis@scrumlaunch.com>
2024-10-25 21:43:58 -0300

    - code cleanup

--- src/pages/Protected/Users/UsersImportInfo.tsx
+++ src/pages/Protected/Users/UsersImportInfo.tsx
@@ -1,44 +1,17 @@
-import { FilterFilled, SearchOutlined } from '@ant-design/icons'
-import { GetProp, InputRef, Table, TableColumnType, TableProps } from 'antd'
+import { Table } from 'antd'
 import Breadcrumb from 'antd/es/breadcrumb'
-import { FilterDropdownProps, SorterResult } from 'antd/es/table/interface'
-import { useRef, useState } from 'react'
-import { ReactSVG } from 'react-svg'
+import { ReactElement, useState } from 'react'
 
 import UsersReviewUpdateModal from '@/pages/Protected/Users/components/UsersReviewUpdateModal'
 
 import { MonroeBlueText } from '@/components/Elements'
 import { Container, Description, Title } from '@/components/Elements/deletingBlockingInfoElements'
-import CellText from '@/components/Table/CellText'
-import FilterDropDown from '@/components/Table/FilterDropDown'
-import MonroeFilter from '@/components/Table/MonroeFilter'
-import TagType from '@/components/Table/TagType'
-import TextWithTooltip from '@/components/TextWithTooltip'
-
 import BaseLayout from '@/layouts/BaseLayout'
 
 import { useUserSlice } from '@/redux/hooks/useUserSlice'
 
-import { getIconColor } from '@/utils'
-
-import { SHORT_GENDER_NAMES } from '@/common/constants'
 import { PATH_TO_USERS } from '@/common/constants/paths'
-import { IImportUsersCSVTableData } from '@/common/interfaces/user'
-import { TGender, TSortOption } from '@/common/types'
-
-import SyncIcon from '@/assets/icons/sync.svg'
-
-type TColumns<T> = TableProps<T>['columns']
-type TTablePaginationConfig = Exclude<GetProp<TableProps, 'pagination'>, boolean>
-
-interface ITableParams {
-  pagination?: TTablePaginationConfig
-  sortField?: SorterResult<IImportUsersCSVTableData>['field']
-  sortOrder?: SorterResult<IImportUsersCSVTableData>['order']
-  filters?: Parameters<GetProp<TableProps, 'onChange'>>[1]
-}
-
-type TDataIndex = keyof IImportUsersCSVTableData
+import { useUsersImportInfoTableParams } from '@/pages/Protected/Users/hooks/useUsersImportInfoTableParams.tsx'
 
 const BREADCRUMB_ITEMS = [
   {
@@ -49,149 +22,41 @@ const BREADCRUMB_ITEMS = [
   },
 ]
 
-const UsersImportInfo = () => {
+/**
+ * The `UsersImportInfo` page displays a list of imported user records (from a CSV file)
+ * and highlights duplicate or erroneous records found during the import process.
+ * This component utilizes the custom `useUsersImportInfoTableParams` hook to manage table state
+ * and configurations, providing a user-friendly overview of the data.
+ *
+ * ## Features:
+ * - Renders a table with imported user records.
+ * - Flags duplicate entries and errors within the dataset.
+ * - Let users review and update the records they choose.
+ *
+ * @component
+ *
+ * @example
+ * // Example usage in a parent component
+ * <UsersImportInfo />
+ *
+ * @returns {ReactElement} A table view of the imported users, with duplicate and error flags.
+ *
+ * Dependencies:
+ * - Ant Design's `Table` component for data rendering.
+ * - `useUsersImportInfoTableParams` custom hook to manage table parameters and configurations.
+ */
+const UsersImportInfo = (): ReactElement => {
   const [selectedIdx, setSelectedIdx] = useState<number | null>(null)
+
   const { importCSVTableRecords } = useUserSlice()
-  const [tableParams, setTableParams] = useState<ITableParams>({
-    pagination: {
-      current: 1,
-      pageSize: 10,
-      pageSizeOptions: [5, 10, 30, 50],
-      showQuickJumper: true,
-      showSizeChanger: true,
-      total: importCSVTableRecords.length,
-    },
+  const {
+    columns,
+    tableParams,
+    handleTableChange
+  } = useUsersImportInfoTableParams({
+    setSelectedIdx,
+    records: importCSVTableRecords
   })
-  const searchInput = useRef<InputRef>(null)
-  const [sortSeasonNameOrder, setSortSeasonNameOrder] = useState<TSortOption>(null)
-  const [sortLeagueNameOrder, setSortLeagueNameOrder] = useState<TSortOption>(null)
-
-  const handleReset = (clearFilters: () => void) => clearFilters()
-
-  const handleSearch = (confirm: FilterDropdownProps['confirm']) => confirm()
-
-  const getColumnSearchProps = (dataIndex: TDataIndex): TableColumnType<IImportUsersCSVTableData> => ({
-    filterDropdown: (props) => (
-      <FilterDropDown {...props} handleReset={handleReset} handleSearch={handleSearch} searchInput={searchInput} />
-    ),
-    filterIcon: (filtered: boolean) => <SearchOutlined style={{ color: filtered ? '#1A1657' : '#BDBCC2' }} />,
-    onFilter: (value, record) =>
-      record[dataIndex]
-        .toString()
-        .toLowerCase()
-        .includes((value as string).toLowerCase()),
-    onFilterDropdownOpenChange: (visible) => {
-      if (visible) {
-        setTimeout(() => searchInput.current?.select(), 100)
-      }
-    },
-  })
-
-  const handleTableChange: TableProps<IImportUsersCSVTableData>['onChange'] = (pagination, _, sorter) => {
-    setTableParams({
-      pagination: {
-        ...pagination,
-      },
-    })
-
-    if (!Array.isArray(sorter) && sorter.field === 'name') setSortSeasonNameOrder(sorter.order || null)
-    if (!Array.isArray(sorter) && sorter.field === 'leagueName') setSortLeagueNameOrder(sorter.order || null)
-  }
-
-  // const handleUpdate = (idx: number) => {
-  //   return idx
-  // }
-
-  const columns: TColumns<IImportUsersCSVTableData> = [
-    {
-      title: 'First Name',
-      dataIndex: 'firstName',
-      width: '240px',
-      sorter: (s1, s2) => s1.firstName.localeCompare(s2.firstName),
-      sortOrder: sortSeasonNameOrder,
-      ...getColumnSearchProps('firstName'),
-      render: (value, record) => (
-        <CellText
-          isLink
-          onClick={() => {
-            record.status === 'Duplicate' && setSelectedIdx(record.idx)
-          }}
-        >
-          {value}
-        </CellText>
-      ),
-    },
-    {
-      title: 'Last Name',
-      dataIndex: 'lastName',
-      width: '240px',
-      sortOrder: sortLeagueNameOrder,
-      sorter: (s1, s2) => s1.lastName.localeCompare(s2.lastName),
-      ...getColumnSearchProps('lastName'),
-      render: (value, record) => (
-        <CellText
-          isLink
-          onClick={() => {
-            record.status === 'Duplicate' && setSelectedIdx(record.idx)
-          }}
-        >
-          {value}
-        </CellText>
-      ),
-    },
-    {
-      title: '',
-      dataIndex: 'gender',
-      width: '80px',
-      onFilter: (value, record) => value === record.gender,
-      render: (value) => <CellText isLink>{SHORT_GENDER_NAMES[value as TGender]}</CellText>,
-      filters: [
-        { text: 'Male', value: 1 },
-        { text: 'Female', value: 0 },
-        { text: 'Other', value: 2 },
-      ],
-      filterDropdown: MonroeFilter,
-      filterIcon: (filtered) => (
-        <FilterFilled
-          style={{
-            color: getIconColor(filtered),
-          }}
-        />
-      ),
-    },
-    {
-      title: 'Status',
-      dataIndex: 'status',
-      width: '132px',
-      filters: [
-        { text: 'Duplicate', value: 'Duplicate' },
-        { text: 'Error', value: 'Error' },
-      ],
-      onFilter: (value, record) => value === record.status,
-      filterIcon: (filtered) => (
-        <FilterFilled
-          style={{
-            color: getIconColor(filtered),
-          }}
-        />
-      ),
-      render: (value) => <TagType text={value} />,
-    },
-    {
-      title: 'Error info',
-      dataIndex: 'message',
-      render: (value) => <TextWithTooltip maxLength={100} text={value} />,
-    },
-    {
-      title: '',
-      dataIndex: '',
-      width: '80px',
-      render: (_, record) =>
-        record.status === 'Duplicate' && (
-          <ReactSVG className="c-p" src={SyncIcon} onClick={() => setSelectedIdx(record.idx)} />
-        ),
-    },
-  ]
 
   return (
     <>
---
+++ src/pages/Protected/Users/hooks/useUsersImportInfoTableParams.tsx
@@ -0,0 +1,183 @@
+import { useTableSearch } from '@/hooks/useTableSearch.tsx'
+import { IImportUsersCSVTableData } from '@/common/interfaces/user.ts'
+import CellText from '@/components/Table/CellText.tsx'
+import { SHORT_GENDER_NAMES } from '@/common/constants'
+import { TGender } from '@/common/types'
+import MonroeFilter from '@/components/Table/MonroeFilter.tsx'
+import { FilterFilled } from '@ant-design/icons'
+import { getIconColor } from '@/utils'
+import TagType from '@/components/Table/TagType.tsx'
+import TextWithTooltip from '@/components/TextWithTooltip.tsx'
+import { ReactSVG } from 'react-svg'
+import SyncIcon from '@/assets/icons/sync.svg'
+import { GetProp, TableProps } from 'antd'
+import { useEffect, useState } from 'react'
+import { SorterResult } from 'antd/es/table/interface'
+
+interface IParams {
+  setSelectedIdx(idx: number): void
+  records: IImportUsersCSVTableData[]
+}
+
+interface ITableParams {
+  pagination?: TTablePaginationConfig
+  sortField?: SorterResult<IImportUsersCSVTableData>['field']
+  sortOrder?: SorterResult<IImportUsersCSVTableData>['order']
+  filters?: Parameters<GetProp<TableProps, 'onChange'>>[1]
+}
+
+type TTablePaginationConfig = Exclude<GetProp<TableProps, 'pagination'>, boolean>
+type TColumns<T> = TableProps<T>['columns']
+
+/**
+ * This hook handles the configuration of importing user csv table and manages its state,
+ * including sorting, pagination, and selection of records.
+
+ * @param {Object} params - The parameters for configuring the table.
+ * @param {function} params.setSelectedIdx - A function to set the selected index of the record.
+ * @param {IImportUsersCSVTableData[]} params.records - An array of records to be displayed in the table.
+ *
+ * @returns An object containing the following properties:
+ *  - {Array} columns - An array of column definitions for the Ant Design table.
+ *  - {function} handleTableChange - A function to update the table parameters (e.g., pagination, sorting).
+ *  - {Object} tableParams - The current table parameters used for managing table state.
+ *
+ * @example
+ * const { columns, setTableParams, tableParams } = useUsersImportInfoTableParams<TObjectShape>({
+ *   setSelectedIdx: (idx) => console.log(`Selected index: ${idx}`),
+ *   records: userData,
+ * });
+ *
+ */
+export const useUsersImportInfoTableParams = ({
+  setSelectedIdx,
+  records,
+}: IParams) => {
+  const { getColumnSearchProps } = useTableSearch()
+
+  const [tableParams, setTableParams] = useState<ITableParams>({
+    pagination: {
+      current: 1,
+      pageSize: 10,
+      pageSizeOptions: [5, 10, 30, 50],
+      showQuickJumper: true,
+      showSizeChanger: true,
+      total: records.length,
+    },
+  })
+
+  // updates pagination
+  useEffect(() => {
+    setTableParams({
+      pagination: {
+        ...tableParams.pagination,
+        total: records.length,
+      },
+    })
+  }, [records])
+
+  const handleTableChange: TableProps<IImportUsersCSVTableData>['onChange'] = (pagination) => {
+    setTableParams({
+      pagination: {
+        ...pagination,
+      },
+    })
+  }
+
+  const columns: TColumns<IImportUsersCSVTableData> = [
+    {
+      title: 'First Name',
+      dataIndex: 'firstName',
+      width: '240px',
+      sorter: (s1, s2) => s1.firstName.localeCompare(s2.firstName),
+      // sortOrder: sortFirstNameOrder,
+      ...getColumnSearchProps<IImportUsersCSVTableData>('firstName'),
+      render: (value, record) => (
+        <CellText
+          isLink
+          onClick={() => {
+            record.status === 'Duplicate' && setSelectedIdx(record.idx)
+          }}
+        >
+          {value}
+        </CellText>
+      )
+    },
+    {
+      title: 'Last Name',
+      dataIndex: 'lastName',
+      width: '240px',
+      // sortOrder: sortLastNameOrder,
+      sorter: (s1, s2) => s1.lastName.localeCompare(s2.lastName),
+      ...getColumnSearchProps('lastName'),
+      render: (value, record) => (
+        <CellText
+          isLink
+          onClick={() => {
+            record.status === 'Duplicate' && setSelectedIdx(record.idx)
+          }}
+        >
+          {value}
+        </CellText>
+      )
+    },
+    {
+      title: '',
+      dataIndex: 'gender',
+      width: '80px',
+      onFilter: (value, record) => value === record.gender,
+      render: (value) => <CellText> {SHORT_GENDER_NAMES[value as TGender]}</CellText>,
+      filters: [
+        { text: 'Female', value: 0 },
+        { text: 'Male', value: 1 },
+        { text: 'Other', value: 2 },
+      ],
+      filterDropdown: MonroeFilter,
+      filterIcon: (filtered) => (
+        <FilterFilled
+          style={{
+            color: getIconColor(filtered)
+          }}
+        />
+      )
+    },
+    {
+      title: 'Status',
+      dataIndex: 'status',
+      width: '132px',
+      filters: [
+        { text: 'Duplicate', value: 'Duplicate' },
+        { text: 'Error', value: 'Error' }
+      ],
+      onFilter: (value, record) => value === record.status,
+      filterIcon: (filtered) => (
+        <FilterFilled
+          style={{
+            color: getIconColor(filtered)
+          }}
+        />
+      ),
+      render: (value) => <TagType text={value} />
+    },
+    {
+      title: 'Error info',
+      dataIndex: 'message',
+      render: (value) => <TextWithTooltip maxLength={100} text={value} />
+    },
+    {
+      title: '',
+      dataIndex: '',
+      width: '80px',
+      render: (_, record) =>
+        record.status === 'Duplicate' && (
+          <ReactSVG className="c-p" src={SyncIcon} onClick={() => setSelectedIdx(record.idx)} />
+        )
+    }
+  ]
+
+  return {
+    columns,
+    handleTableChange,
+    tableParams
+  }
+}
