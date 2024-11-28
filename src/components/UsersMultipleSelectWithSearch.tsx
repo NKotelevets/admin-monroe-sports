b1107e736839b242ab368a0ba149bfe456b6ccd0
Bruno Serra Reis <bruno.reis@scrumlaunch.com>
2024-11-28 19:44:58 -0300

    - code analysis suggestions

--- src/components/UsersMultipleSelectWithSearch.tsx
+++ src/components/UsersMultipleSelectWithSearch.tsx
@@ -61,7 +61,7 @@ const List = styled.ul`
   box-shadow:
     0 3px 6px -4px rgba(0, 0, 0, 0.12),
     0 6px 16px 0 rgba(0, 0, 0, 0.08),
-    0px 9px 28px 8px rgba(0, 0, 0, 0.05);
+    0 9px 28px 8px rgba(0, 0, 0, 0.05);
   padding-right: 4px;
   z-index: 20;
   overflow: scroll;
