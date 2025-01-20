4b6a972965dfccc9743712db38ce47ec07d7dd28
Bruno Serra Reis <bruno.reis@scrumlaunch.com>
2025-01-20 17:36:58 -0300

    Event Type | Need change color for all tags

--- src/pages/Protected/Events/components/EventTypeTag.tsx
+++ src/pages/Protected/Events/components/EventTypeTag.tsx
@@ -1,10 +1,13 @@
 import { Tag } from 'antd'
 import { ReactElement } from 'react'
 import { eventType } from '@/common/constants/events.ts'
+import styled from '@emotion/styled'
 
 type TEventTypeTagProps = {
   type: number
 }
+
+type TColorMap = { color: string; name: string; bg: string; border: string }
 
 /**
  * EventTypeTag Component
@@ -37,16 +40,41 @@ type TEventTypeTagProps = {
 export const EventTypeTag = (props: TEventTypeTagProps): ReactElement => {
   const { type } = props
 
-  const typeMap: Record<number, { color: string; name: string }> = {
-    [eventType.GAME]: { color: 'green', name: 'Game' },
-    [eventType.PRACTICE]: { color: 'blue', name: 'Practice' },
-    [eventType.PLAYOFF]: { color: 'default', name: 'Playoff' },
-    [eventType.OTHER]: { color: 'yellow', name: 'Other event' },
+  const typeMap: Record<number, TColorMap> = {
+    [eventType.GAME]: {
+      bg: 'rgba(241, 250, 239, 1)',
+      border: 'rgba(158, 224, 148, 1)',
+      color: 'rgba(16, 177, 22, 1)',
+      name: 'Game'
+    },
+    [eventType.PRACTICE]: {
+      bg: 'rgba(241, 240, 255, 1)',
+      border: 'rgba(164, 158, 255, 1)',
+      color: 'rgba(76, 65, 230, 1)',
+      name: 'Practice'
+    },
+    [eventType.PLAYOFF]: {
+      bg: 'rgba(250, 250, 250, 1)',
+      border: 'rgba(216, 215, 219, 1)',
+      color: 'rgba(26, 22, 87, 0.85)',
+      name: 'Playoff'
+    },
+    [eventType.OTHER]: {
+      bg: 'rgba(255, 249, 235, 1)',
+      border: 'rgba(255, 215, 112, 1)',
+      color: 'rgba(163, 119, 5, 1)',
+      name: 'Other event' },
   }
 
   if (type in typeMap) {
-    return <Tag color={typeMap[type].color}>{typeMap[type].name}</Tag>
+    return <TagStyled {...typeMap[type]}>{typeMap[type].name}</TagStyled>
   }
 
-  return <Tag color={typeMap[5].color}>{typeMap[5].name}</Tag>
+  return <TagStyled {...typeMap[type]}>{typeMap[5].name}</TagStyled>
 }
+
+const TagStyled = styled(Tag)<TColorMap>`
+    background-color: ${({ bg }) => bg} !important;
+    color: ${({ color }) => color} !important;
+    border-color: ${({ border }) => border} !important;
+`
