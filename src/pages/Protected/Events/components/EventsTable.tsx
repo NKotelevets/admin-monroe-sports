5d5f2937a0737ef27d5183cd7563688aeedcfcd8
Bruno Serra Reis <bruno.reis@scrumlaunch.com>
2025-01-21 18:32:42 -0300

    Main Table | Need to send a valid date for BE

--- src/pages/Protected/Events/components/EventsTable.tsx
+++ src/pages/Protected/Events/components/EventsTable.tsx
@@ -114,7 +114,7 @@ export const EventsTable = () => {
       limit: newLimit,
       ordering: getTableSortField<IEvent>(sorter, fieldMap),
       date: filters?.['date'] ? filters?.['date']?.join(',') :  undefined,
-      day: (filters?.['day']?.[0] as string) ?? undefined,
+      day: filters?.['day'] ? filters?.['day']?.join(',') : undefined,
       leagueName: (filters?.['leagueName']?.[0] as string) ?? undefined,
       subResource: (filters?.['subResource']?.[0] as string) ?? undefined,
       team1name: (filters?.['homeTeam']?.[0] as string) ?? undefined,
