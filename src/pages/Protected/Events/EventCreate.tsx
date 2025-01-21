b6469e6e7f7516cbd9dad4e0e6aa59026b8b5923
Bruno Serra Reis <bruno.reis@scrumlaunch.com>
2025-01-21 09:40:50 -0300

    fix event repeatEndDate and repeats new values

--- src/pages/Protected/Events/EventCreate.tsx
+++ src/pages/Protected/Events/EventCreate.tsx
@@ -40,7 +40,7 @@ const EventCreate = () => {
       court_or_field: body.courtOrField,
       sub_resource: body.subResources,
       repeats: body.repeats,
-      endRepeat: body.endRepeat,
+      repeatEndDate: body.endRepeat,
       ignore_conflicts: body.ignoreConflicts,
       team_1_id: body.team1Id,
       team_2_id: body.team2Id || null,
--- src/pages/Protected/Events/EventEdit.tsx
+++ src/pages/Protected/Events/EventEdit.tsx
@@ -47,7 +47,7 @@ const EventEdit = () => {
       court_or_field: body.courtOrField,
       sub_resource: body.subResources,
       repeats: body.repeats,
-      endRepeat: body.endRepeat,
+      repeatEndDate: body.endRepeat,
       ignore_conflicts: body.ignoreConflicts,
       team_1_id: body.team1Id,
       team_2_id: body.team2Id || null,
