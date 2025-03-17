2baa151e614daa3189bd2004976ac4f99ee50470
brunoserra <bruno.reis@scrumlaunch.com>
2025-03-17 13:49:12 -0300

    Dynamic message based on role/team variables

--- src/pages/Account/Invitations/components/InvitationDenied.tsx
+++ src/pages/Account/Invitations/components/InvitationDenied.tsx
@@ -14,11 +14,15 @@ type TInvitationDeniedProps = {
 
 const InvitationDenied = (props: TInvitationDeniedProps) => {
   const { teamName, role } = props
+  const phrase1 = ` to join ${teamName}${role ? ` as ${role}` : ''}`
+  const phrase2 = teamName ? ' and would like to join this team' : ''
+  const inviter = teamName ? 'to a Master Team Admin' : 'the inviter'
+
   return (
     <Body centered>
       <Illustration src={PlayerIllustration} />
       <Title>Invitation Rejected</Title>
-      <Subtitle style={{marginBottom: 32}}>You have successfully rejected the invitation to join {teamName}{role ? ` as ${role}` : ''}. If you change your mind and would like to join this team, please reach out to a Master Team Admin to request a new invitation.</Subtitle>
+      <Subtitle style={{marginBottom: 32}}>You have successfully rejected the invitation{phrase1}. If you change your mind{phrase2}, please reach out {inviter} to request a new invitation.</Subtitle>
       <Subtitle>For further support, contact us at <Link to={'mailto:info@swiftschedule.net'}>info@swiftschedule.net</Link>.</Subtitle>
     </Body>
   )
